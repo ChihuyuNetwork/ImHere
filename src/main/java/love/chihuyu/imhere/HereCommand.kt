b4244f2108f0c9828901f784388c988d2e521ef7
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-08-15 02:15:19 +0900

    clean val

--- src/main/java/love/chihuyu/imhere/HereCommand.kt
+++ src/main/java/love/chihuyu/imhere/HereCommand.kt
@@ -11,15 +11,9 @@ object HereCommand: CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender !is Player || !sender.hasPermission("here.command.here")) return true
 
-        if (args.isNotEmpty()) {
-            val player = plugin.server.getPlayer(args[0]) ?: return true
-            plugin.server.onlinePlayers.forEach {
-                it.sendMessage("§7(§6${sender.name}§7) ${player.name}'s Location: ${"%,.1f".format(player.location.x)}, ${"%,.1f".format(player.location.y)}, ${"%,.1f".format(player.location.z)}")
-            }
-        } else {
-            plugin.server.onlinePlayers.forEach {
-                it.sendMessage("§7(§6${sender.name}§7) ${sender.name}'s Location: ${"%,.1f".format(sender.location.x)}, ${"%,.1f".format(sender.location.y)}, ${"%,.1f".format(sender.location.z)}")
-            }
+        val player = if (args.isNotEmpty()) sender else plugin.server.getPlayer(args[0]) ?: return true
+        plugin.server.onlinePlayers.forEach {
+            it.sendMessage("§7(§6${sender.name}§7) ${player.name}'s Location: (${sender.world.name}) ${"%,.1f".format(player.location.x)}, ${"%,.1f".format(player.location.y)}, ${"%,.1f".format(player.location.z)}")
         }
 
         return true
